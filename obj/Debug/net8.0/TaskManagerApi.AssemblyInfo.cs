f813405b8435ffd103e0c9c4cce38bdd09529639
jair quinto <giovanni.quinto02@gmail.com>
2026-05-29 21:02:18 -0500

    Implementa consumo de API externa

--- obj/Debug/net8.0/TaskManagerApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/TaskManagerApi.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TaskManagerApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+2d159ce20dc2e38e82154f8f785689c0b486636d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+da42792e4de33f0f57edb57c8479c5b41017a769")]
 [assembly: System.Reflection.AssemblyProductAttribute("TaskManagerApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TaskManagerApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
